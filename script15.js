400f2d01fd512c6d6e02bdc8fa028507023d1318
N4TH4N-K <nkondabatni@gmail.com>
2025-09-06 19:14:09 -0700

    Update script15.js

---
+++ script15.js
@@ -0,0 +1,323 @@
+const questions15 = [
+    {question: "apt",
+    answers: [
+        { text: "fit", correct: true},
+        { text: "change", correct: false},
+        { text: "hear", correct: false},
+        { text: "walk", correct: false}
+    ],
+    hint: "apt, adapt, aptitude, maladapted, aptly, adaptation" }, {
+    question: "ambul",
+    answers: [
+        { text: "walk", correct: true},
+        { text: "hospital", correct: false},
+        { text: "health", correct: false},
+        { text: "run", correct: false}
+    ],
+    hint: "ambulance, ambulatory, perambulator, somnabulism, funambulist, amble" }, {
+    question: "aud",
+    answers: [
+        { text: "hear", correct: true},
+        { text: "see", correct: false},
+        { text: "taste", correct: false},
+        { text: "touch", correct: false}
+    ],
+    hint: "audiophile, audience, audition, audit, audiology, obedient (?)" },
+    {question: "cand",
+    answers: [
+        { text: "glow / burn", correct: true},
+        { text: "go / yield", correct: false},
+        { text: "kill", correct: false},
+        { text: "lift / raise / rise", correct: false}
+    ],
+    hint: "candle, candor, incandescence, kindle, chandelier, candelabrum" }, {
+        question: "caust",
+        answers: [
+            { text: "burn", correct: true},
+            { text: "glow", correct: false},
+            { text: "kill", correct: false},
+            { text: "blame / fault", correct: false}
+        ],
+        hint: "holocaust, caustic, cauterize, caustically" }, {
+        question: "cede / cess",
+        answers: [
+            { text: "go / yield", correct: true},
+            { text: "lead / bring", correct: false},
+            { text: "throw / cast", correct: false},
+            { text: "blame / fault", correct: false}
+        ],
+        hint: "recede, precede, antecedent, recess, procession, accede, abscess, ancestor, decease, exceed, excess, intercede, secede, recede, proceed, succeed" },
+    {question: "cide",
+        answers: [
+            { text: "kill", correct: true},
+            { text: "owe", correct: false},
+            { text: "cry out", correct: false},
+            { text: "blame / fault", correct: false}
+        ],
+        hint: "herbicide, homicide, patricide, suicide, genocide, matricide, germicide, pesticide" }, {
+        question: "clam / claim",
+        answers: [
+            { text: "cry out", correct: true},
+            { text: "own", correct: false},
+            { text: "flee", correct: false},
+            { text: "throw / cast", correct: false}
+        ],
+        hint: "clamorous, exclamation, clamant, declaim, clamor, acclaim, reclaim" }, {
+        question: "culp",
+        answers: [
+            { text: "blame / fault", correct: true},
+            { text: "owe", correct: false},
+            { text: "cry out", correct: false},
+            { text: "kill", correct: false}
+        ],
+        hint: "culpable, culprit, exculpate, exculpatory, mea culpa, inculpate" },
+    {question: "deb",
+        answers: [
+            { text: "owe", correct: true},
+            { text: "kill", correct: false},
+            { text: "cry out", correct: false},
+            { text: "blame / fault", correct: false}
+        ],
+        hint: "debt, debtor, debit, debenture" }, {
+        question: "duct",
+        answers: [
+            { text: "lead / bring", correct: true},
+            { text: "fix", correct: false},
+            { text: "lift / raise / rise", correct: false},
+            { text: "blame / fault", correct: false}
+        ],
+        hint: "conduct, introduction, product, reduction, reproduction, production, abduction, induct, induction, etc" }, {
+        question: "fer / phor / pher",
+        answers: [
+            { text: "carry", correct: true},
+            { text: "fix", correct: false},
+            { text: "lift / raise / rise", correct: false},
+            { text: "throw / cast", correct: false}
+        ],
+        hint: "transfer, refer, ferry, metaphor, circumference, confer, defer, differ, infer, prefer, suffer, phosphorus, paraphernalia" },
+    {question: "fug",
+        answers: [
+            { text: "flee", correct: true},
+            { text: "kill", correct: false},
+            { text: "lift / raise / rise", correct: false},
+            { text: "blame / fault", correct: false}
+        ],
+        hint: "centrifuge, fugitive, subterfuge, refugee, fugue" }, {
+        question: "grad / gress",
+        answers: [
+            { text: "step", correct: true},
+            { text: "owe", correct: false},
+            { text: "lift / raise / rise", correct: false},
+            { text: "lead / bring", correct: false}
+        ],
+        hint: "gradual, degrading, congress, regress, digress, progress, grade, graduate, degrade, retrograde, aggress, egress, ingress, transgress" }, {
+        question: "ject",
+        answers: [
+            { text: "throw / cast", correct: true},
+            { text: "lead / bring", correct: false},
+            { text: "lift / raise / rise", correct: false},
+            { text: "blame / fault", correct: false}
+        ],
+        hint: "eject, reject, interjection, conjecture, dejected, projection, abject, adjective, inject, jetsam, jetty, jut, object, subject" },
+    {
+        question: "lev",
+        answers: [
+            { text: "flee", correct: false},
+            { text: "kill", correct: false},
+            { text: "lift / raise / rise", correct: true},
+            { text: "blame / fault", correct: false}
+        ],
+        hint: "lever, levy, leaven, levee, elevate, Levant" }, {
+        question: "mem",
+        answers: [
+            { text: "remember", correct: true},
+            { text: "measure", correct: false},
+            { text: "change", correct: false},
+            { text: "wonder", correct: false}
+        ],
+        hint: "memory, memorandum, commemorate, memorial, memento, memoir, remembrance" }, {
+        question: "meta / muta",
+        answers: [
+            { text: "change", correct: true},
+            { text: "wonder", correct: false},
+            { text: "speak", correct: false},
+            { text: "remember", correct: false}
+        ],
+        hint: "metamorphosis, metaphor, metabolism, mutate, mutable, commute, mutual, premutation, transmute" },
+    {question: "meter",
+        answers: [
+            { text: "measure", correct: true},
+            { text: "wonder", correct: false},
+            { text: "speak", correct: false},
+            { text: "graph", correct: false}
+        ],
+        hint: "thermometer, millimeter, centimeter, kilometer, symmetry, odometer, geometry, barometer, chronometer, diameter, perimeter, hexameter, trigonometry" }, {
+        question: "mir",
+        answers: [
+            { text: "wonder", correct: true},
+            { text: "speak", correct: false},
+            { text: "grasp", correct: false},
+            { text: "laugh", correct: false}
+        ],
+        hint: "mirage, miracle, mirror, admire, mirabilia, marvel" }, {
+        question: "parl",
+        answers: [
+            { text: "speak", correct: true},
+            { text: "grasp", correct: false},
+            { text: "wonder", correct: false},
+            { text: "remember", correct: false}
+        ],
+        hint: "parlor, parliament, parley, parlance, parole" }, {
+        question: "prehend",
+        answers: [
+            { text: "speak", correct: false},
+            { text: "grasp", correct: true},
+            { text: "wonder", correct: false},
+            { text: "remember", correct: false}
+        ],
+        hint: "apprehend, comprehend, reprehend, prehensile, impregnable, comprise, enterprise, reprisal, surprise, prison" },
+    {question: "rid / ris",
+        answers: [
+            { text: "laugh", correct: true},
+            { text: "grasp", correct: false},
+            { text: "wonder", correct: false},
+            { text: "speak", correct: false}
+        ],
+        hint: "ridicule, derision, risibility, ridiculous, deride, risible" }, {
+        question: "sequ / sec",
+        answers: [
+            { text: "follow", correct: true},
+            { text: "grasp", correct: false},
+            { text: "wonder", correct: false},
+            { text: "speak", correct: false}
+        ],
+        hint: "sequence, consecutive, sequel, obsequious, subsequent, consequent, exequies, execute, persecute, prosecute" }, {
+        question: "ven / vent",
+        answers: [
+            { text: "come", correct: true},
+            { text: "flow", correct: false},
+            { text: "speak", correct: false},
+            { text: "lift / raise / rise", correct: false}
+        ],
+        hint: "parlor, parliament, parley, parlance, parole" }
+];
+
+const startButton15 = document.getElementById("start14");
+const startScreen = document.getElementById("start-screen");
+const quizScreen = document.getElementById("quiz");
+const questionElement = document.getElementById("question");
+const options = document.getElementById("options");
+const nextButton = document.getElementById("next");
+
+let currentQuestions = [];
+let currentQuestionIndex = 0;
+let score = 0;
+
+const hintButton = document.getElementById("hint-button");
+const hintContent = document.getElementById("hint-content");
+const hintText = document.getElementById("hint-text");
+
+hintButton.addEventListener("click", () => {
+    hintContent.style.display = "block";
+});
+
+startButton15.addEventListener("click", () => {
+    startScreen.style.display = "none";
+    quizScreen.style.display = "block";
+    startQuiz();
+});
+
+function startQuiz() {
+    currentQuestions = shuffle([...questions15]);
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next &#8594;";
+    nextButton.style.display = "none";
+    showQuestion();
+}
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function showQuestion() {
+    resetState();
+
+    const currentQuestion = currentQuestions[currentQuestionIndex];
+    questionElement.innerHTML = `${currentQuestionIndex + 1}. ${currentQuestion.question}`;
+    hintContent.style.display = "none"; // Hide hint content again
+    hintText.innerText = currentQuestion.hint || "";
+
+    shuffle(currentQuestion.answers).forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        if (answer.correct) button.dataset.correct = answer.correct;
+        button.addEventListener("click", selectAnswer);
+        options.appendChild(button);
+    });
+}
+
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (options.firstChild) {
+        options.removeChild(options.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    } else {
+        selectedBtn.classList.add("incorrect");
+        const correctBtn = Array.from(options.children).find(btn => btn.dataset.correct === "true");
+        if (correctBtn) correctBtn.classList.add("correct");
+    }
+
+    Array.from(options.children).forEach(button => button.disabled = true);
+
+    nextButton.style.display = "block";
+}
+
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < currentQuestions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+});
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of 25!`;
+    nextButton.style.display = "none";
+
+    const playAgain = document.createElement("button");
+    playAgain.innerHTML = "Play Again";
+    playAgain.classList.add("btn");
+    playAgain.addEventListener("click", () => { startQuiz(currentQuestions); });
+    options.appendChild(playAgain);
+
+    const homePage = document.createElement("button");
+    homePage.innerHTML = "Home &#9166;";
+    homePage.classList.add("btn");
+    homePage.addEventListener("click", () => { window.location.href = "file:///C:/Users/nkond/Downloads/Stems-List/index.html"; });
+    options.appendChild(homePage);
+
+    const nextList = document.createElement("button");
+    nextList.innerHTML = "Next List &#8594;";
+    nextList.classList.add("btn");
+    nextList.addEventListener("click", () => { window.location.href ="file:///C:/Users/nkond/Downloads/Stems-List/list14.html"; });
+    options.appendChild(nextList);
+}
+
